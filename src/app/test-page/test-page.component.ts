962fbbdbc70fcba950f141ccc5b99c96ab5c3453
skidan29 <63918439+skidan29@users.noreply.github.com>
2022-03-25 15:01:18 +0300

    закончил на анимации раздел trasition

--- src/app/test-page/test-page.component.ts
+++ src/app/test-page/test-page.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {
+    animate,
+    state,
+    style,
+    transition,
+    trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-test-page',
   templateUrl: './test-page.component.html',
   styleUrls: ['./test-page.component.scss'],
+    animations:[
+        trigger('box',[
+            state('start', style({background:'blue'})),
+            state('end', style({background: 'red', transform: 'scale(1.2)'})),
+            transition('start=>end',animate(450)),
+            transition('end=>start',animate(450))
+        ])
+    ]
 })
 export class TestPageComponent implements OnInit {
+    public boxState = 'start'
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  animate(){
+        this.boxState = this.boxState === 'start' ? 'end': 'start';
   }
 
 }
