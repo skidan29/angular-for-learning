6314d7885a052021f916e376b7c5b334ff812156
skidan29 <63918439+skidan29@users.noreply.github.com>
2022-03-25 13:01:09 +0300

    Начало изучения раздела анимации

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StyleDirective } from './directives/style.directive';
 import { TestPageComponent } from './test-page/test-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { TestPageComponent } from './test-page/test-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
